610c1f9ca46264596d060aa165a08e01337b2edd
Mostafa S. El-bastawisy <mostafa.sayed112@gmail.com>
2021-09-20 14:01:22 +0200

    Finaly

--- main.js
+++ main.js
@@ -44,3 +44,84 @@ allImages.forEach((img) => {
 	img.alt = "Elzero Logo";
 });
 
+console.log("#".repeat(77));
+
+const inputFeild = document.querySelector("form input");
+const result = document.querySelector("form .result");
+
+inputFeild.addEventListener("input", () => {
+	if (inputFeild.value >= 0) {
+		let eygptCurrency = inputFeild.value * 15.6;
+		result.textContent = `{${inputFeild.value || 0}} USD Dollar = {${
+			inputFeild.value === "" ? 0 : eygptCurrency.toFixed(2)
+		}} Egyption Pound`;
+	}
+});
+
+console.log("#".repeat(77));
+
+let firstDiv = document.querySelector(".one");
+let secondDiv = document.querySelector(".two");
+let secondDivTitle = secondDiv.title;
+let secondDivText = secondDiv.textContent;
+
+secondDiv.title = firstDiv.title;
+firstDiv.title = secondDivTitle;
+secondDiv.textContent = `${firstDiv.textContent} 2`;
+firstDiv.textContent = secondDivText;
+
+console.log("#".repeat(77));
+
+
+let allImgs = document.querySelectorAll(".old");
+
+allImgs.forEach((older) => {
+	if (older.hasAttribute("alt")) {
+		older.alt = "Old";
+	} else {
+		older.alt = "Elzero New";
+	}
+});
+
+console.log("#".repeat(77));
+
+
+let app = () => {
+	const form = document.forms[0];
+	const numOfElements = document.querySelector("form input[type='number']");
+	const text = document.querySelector("form input[type='text']");
+	const type = document.querySelector("form select");
+	const result = document.querySelector("form .results");
+
+	form.onsubmit = (event) => {
+		// Prevent the default act for a form when submit it (refresh and empty the fields).
+		event.preventDefault();
+
+		// Remove all childs of the result element.
+		document.querySelectorAll(".results .box").forEach((box) => box.remove());
+
+		if (numOfElements.value !== "" && text.value.trim() !== "") {
+			if (numOfElements.value > 0) {
+				for (let i = 0; i < numOfElements.value; i++) {
+					let box = document.createElement(type.value);
+					box.className = "box";
+					box.title = "Element";
+					box.id = `id-${i + 1}`;
+					let boxText = document.createTextNode(text.value);
+
+					box.appendChild(boxText);
+					result.appendChild(box);
+				}
+			} else alert("Number Of Elements Should Be Greater Than 0");
+		} else alert("Please fill in all the required fields.");
+	};
+};
+
+app();
+
+
+
+
+
+
+
